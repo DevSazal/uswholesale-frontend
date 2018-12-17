f55be090bcb7e3ef5b232a014add8dbaeeb67753
ishraqe <kanak431@diu.edu.bd>
2018-12-17 23:13:27 +0600

    checkbox added

--- js/custom.js
+++ js/custom.js
@@ -1,49 +1,72 @@
 (function($) {
   $.fn.menumaker = function(options) {
     var cssmenu = $(this),
-      settings = $.extend({
-        format: "dropdown",
-        sticky: false
-      }, options);
+      settings = $.extend(
+        {
+          format: "dropdown",
+          sticky: false
+        },
+        options
+      );
     return this.each(function() {
-      $(this).find(".button").on('click', function() {
-        $(this).toggleClass('menu-opened');
-        var mainmenu = $(this).next('ul');
-        if (mainmenu.hasClass('open')) {
-          mainmenu.slideToggle().removeClass('open');
-        } else {
-          mainmenu.slideToggle().addClass('open');
-          if (settings.format === "dropdown") {
-            mainmenu.find('ul').show();
-          }
-        }
-      });
-      cssmenu.find('li ul').parent().addClass('has-sub');
-      multiTg = function() {
-        cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
-        cssmenu.find('.submenu-button').on('click', function() {
-          $(this).toggleClass('submenu-opened');
-          if ($(this).siblings('ul').hasClass('open')) {
-            $(this).siblings('ul').removeClass('open').slideToggle();
+      $(this)
+        .find(".button")
+        .on("click", function() {
+          $(this).toggleClass("menu-opened");
+          var mainmenu = $(this).next("ul");
+          if (mainmenu.hasClass("open")) {
+            mainmenu.slideToggle().removeClass("open");
           } else {
-            $(this).siblings('ul').addClass('open').slideToggle();
+            mainmenu.slideToggle().addClass("open");
+            if (settings.format === "dropdown") {
+              mainmenu.find("ul").show();
+            }
+          }
+        });
+      cssmenu
+        .find("li ul")
+        .parent()
+        .addClass("has-sub");
+      multiTg = function() {
+        cssmenu
+          .find(".has-sub")
+          .prepend('<span class="submenu-button"></span>');
+        cssmenu.find(".submenu-button").on("click", function() {
+          $(this).toggleClass("submenu-opened");
+          if (
+            $(this)
+              .siblings("ul")
+              .hasClass("open")
+          ) {
+            $(this)
+              .siblings("ul")
+              .removeClass("open")
+              .slideToggle();
+          } else {
+            $(this)
+              .siblings("ul")
+              .addClass("open")
+              .slideToggle();
           }
         });
       };
-      if (settings.format === 'multitoggle') multiTg();
-      else cssmenu.addClass('dropdown');
-      if (settings.sticky === true) cssmenu.css('position', 'fixed');
+      if (settings.format === "multitoggle") multiTg();
+      else cssmenu.addClass("dropdown");
+      if (settings.sticky === true) cssmenu.css("position", "fixed");
       resizeFix = function() {
         var mediasize = 1000;
         if ($(window).width() > mediasize) {
-          cssmenu.find('ul').show();
+          cssmenu.find("ul").show();
         }
         if ($(window).width() <= mediasize) {
-          cssmenu.find('ul').hide().removeClass('open');
+          cssmenu
+            .find("ul")
+            .hide()
+            .removeClass("open");
         }
       };
       resizeFix();
-      return $(window).on('resize', resizeFix);
+      return $(window).on("resize", resizeFix);
     });
   };
 })(jQuery);
@@ -55,3 +78,16 @@
     });
   });
 })(jQuery);
+
+// if no Webkit browser
+(function() {
+  let isChrome =
+    /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  let isSafari =
+    /Safari/.test(navigator.userAgent) &&
+    /Apple Computer/.test(navigator.vendor);
+  let scrollbarDiv = document.querySelector(".scrollbar");
+  if (!isChrome && !isSafari) {
+    scrollbarDiv.innerHTML = "You need Webkit browser to run this code";
+  }
+})();
